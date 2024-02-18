cbf2789bf7ca2c9b19fdf2ccfb49c34eb207b998
EverCR1 <evermcr11@gmail.com>
2024-02-18 00:09:46 -0600

    Proyecto Subido

---
+++ AppOchoReinas/Metodos.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOchoReinas
+{
+    public class Metodos
+    {
+        public const int BoardSize = 8; //Tamaño del tablero (8x8)
+        public const int SquareSize = 60; //Tamaño por cada cuadro
+        private List<List<int>> solutions; //Lista de soluciones
+        private int currentSolutionIndex; //Indice de la solución
+
+        public Metodos()
+        {
+            solutions = new List<List<int>>();
+            ResolverProblema(new List<int>(), 0); //Inicia la solución
+        }
+
+        public List<List<int>> Solutions => solutions;
+
+        //Accede y modifica el indice actual
+        public int CurrentSolutionIndex
+        {
+            get => currentSolutionIndex;
+            set => currentSolutionIndex = value;
+        }
+
+        public void ResolverProblema(List<int> queens, int row)
+        {
+            if (row == BoardSize)
+            {
+                solutions.Add(new List<int>(queens)); //Agrega una solución encontrada a la lista
+                return;
+            }
+
+            for (int col = 0; col < BoardSize; col++)
+            {
+                if (validarPosicion(queens, row, col))
+                {
+                    queens.Add(col);
+                    ResolverProblema(queens, row + 1); //Aplicación de recursividad para buscar soluciones
+                    queens.RemoveAt(queens.Count - 1);
+                }
+            }
+        }
+
+        public bool validarPosicion(List<int> queens, int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                if (queens[i] == col || queens[i] - i == col - row || queens[i] + i == col + row)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Dibujar el tablero
+        public void DrawBoard(Graphics g)
+        {
+            
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    Brush brush = (i + j) % 2 == 0 ? Brushes.LightBlue : Brushes.White;
+                    g.FillRectangle(brush, j * SquareSize, i * SquareSize, SquareSize, SquareSize);
+                }
+            }
+        }
+
+        //Dibujar las reinas
+        public void DrawQueens(Graphics g, List<int> queens)
+        {
+            for (int i = 0; i < queens.Count; i++)
+            {
+                int x = queens[i] * SquareSize;
+                int y = i * SquareSize;
+                g.FillEllipse(Brushes.Blue, x, y, SquareSize, SquareSize);
+            }
+        }
+    }
+}
---
+++ AppOchoReinas/frmPrincipal.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppOchoReinas
+{
+    public partial class frmPrincipal : Form
+    {
+        private Metodos metodos;
+
+        private Button btnNext;
+        private Button btnPrev;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            metodos = new Metodos();
+            InitializeBoard();
+            InitializeButtons();
+            ShowSolution(0);
+        }
+
+        //Inicializa los botones
+        private void InitializeButtons()
+        {
+            btnNext = new Button();
+            btnNext.Text = "Siguiente";
+            btnNext.Location = new Point(250, Metodos.BoardSize * Metodos.SquareSize + 20);
+            btnNext.Click += BtnNext_Click;
+            this.Controls.Add(btnNext);
+
+            btnPrev = new Button();
+            btnPrev.Text = "Anterior";
+            btnPrev.Location = new Point(150, Metodos.BoardSize * Metodos.SquareSize + 20);
+            btnPrev.Click += BtnPrev_Click;
+            this.Controls.Add(btnPrev);
+        }
+
+        //Evento botón Siguiente
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            ShowNextSolution();
+        }
+
+        //Evento botón Anterior
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            ShowPreviousSolution();
+        }
+
+        //Muestra la siguiente solución en la interfaz gráfica
+        private void ShowNextSolution()
+        {
+            if (metodos.CurrentSolutionIndex < metodos.Solutions.Count - 1)
+            {
+                metodos.CurrentSolutionIndex++;
+                ShowSolution(metodos.CurrentSolutionIndex);
+            }
+        }
+
+        //Muestra la solución anterior en la interfaz
+        private void ShowPreviousSolution()
+        {
+            if (metodos.CurrentSolutionIndex > 0)
+            {
+                metodos.CurrentSolutionIndex--;
+                ShowSolution(metodos.CurrentSolutionIndex);
+            }
+        }
+
+        //Muestra una solución específica en la interfaz
+        private void ShowSolution(int index)
+        {
+            metodos.CurrentSolutionIndex = index;
+
+            if (index >= 0 && index < metodos.Solutions.Count)
+            {
+                this.Text = $"Ever CR - Problema de las 8 Reinas - Solución {index + 1}/{metodos.Solutions.Count}";
+                this.Invalidate();
+            }
+            else
+            {
+                this.Text = "Sin soluciones"; //Si no hay soluciones, muestra mensaje de error
+            }
+        }
+
+        //Inicializa el tamaño del formulario basándose en el tamaño del tablero
+        private void InitializeBoard()
+        {
+            this.Size = new Size(Metodos.BoardSize * Metodos.SquareSize + 20, Metodos.BoardSize * Metodos.SquareSize + 100);
+            this.Paint += PaintForm;
+        }
+
+        //Maneja el evento de pintura del formulario
+        private void PaintForm(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            if (metodos.Solutions.Count > 0)
+            {
+                metodos.DrawBoard(g);
+                metodos.DrawQueens(g, metodos.Solutions[metodos.CurrentSolutionIndex]);
+            }
+        }
+    }
+}
+
+
